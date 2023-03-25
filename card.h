47cb42157a4381710e164d6915970cb266bfdc0b
Shepherd23333 <927561263l@gmail.com>
2023-03-25 16:55:30 +0800

    improved

--- card.h
+++ card.h
@@ -7,35 +7,44 @@
 #include<stdbool.h>
 struct Time{
     int year, month, day, hour, minute;
-};
+};  //时间
 typedef struct Time Time;
+
 struct Log{
     Time time;
     float money;
     struct Log *next;
-};
+};  //交易记录
 typedef struct Log Log;
 Log* logCreate();
 Log* logDelete(Log*, Time);
 Log* logExtend(Log*);
 Log* logSearch(Log*, Time);
+
 struct card{
     char *name;
     char *number;
+    char *password; //密码
     short level;
     float remaining_sum,comsumption;  //余额，消费额
     Time createTime,validTime;  //创建时间，有效时间
     bool isLost;
-    Log *rechargeLog,*comsumeLog;
+    Log *rechargeLog,*comsumeLog;   //充值、消费记录
     struct Card *next;
 };
 typedef struct Card Card;
 Card* cardCreate();
-Card* cardDelete(Card*, char*);
+Card* cardDelete(Card*, char*);     //按卡号或用户名字删除，即退卡等
 Card* cardExtend(Card*);
-void cardSort(Card*);
-Card* cardSearch(Card*, char*);
-void cardRecharge(Card*, float);
-void cardComsume(Card*, float);
+void cardSort(Card*);   //依次按关键词排序
+Card* cardFix(Card*);   //信息修改
+Card* cardSearch(Card*, char*);    //按卡号、用户名字查找
+bool cardLogIn(char*, char*); //会员卡登录
+void cardRecharge(Card*, float);    //充值续费，升级
+void cardComsume(Card*, float);     //消费，打折
+void cardFind(Card*, char*); //找回密码
+
+void backUp();  //数据备份
+void restore(); //数据恢复
 
 #endif
